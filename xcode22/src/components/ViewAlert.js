148337660117a27875e081467383069cc309c6bd
deveshkedia <dkediaphone@icloud.com>
2022-10-16 08:36:19 +0530

    Everything working oin this one

--- xcode22/src/components/ViewAlert.js
+++ xcode22/src/components/ViewAlert.js
@@ -3,41 +3,41 @@ import React, { useState } from 'react'
 import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native'
 
 const styles = StyleSheet.create({
-  AlertFieldContainer: {
-    flexDirection: 'column',
-    width: '90%',
-    margin: '1.5%',
-    paddingHorizontal: 20,
-    paddingVertical: 7,
-    backgroundColor: '#E6F1FF',
-    alignItems: 'center',
-    borderRadius: 15,
-  },
-  fieldTitle: {
-    fontSize: 20,
-    marginBottom: 10,
-    textDecorationLine: 'underline',
-  },
-  descriptionField: {
-    fontSize: 15,
-  },
-  alertDetails: {
-    flexDirection: 'column',
-    fontSize: 10,
-    color: '#8892B0',
-    alignItems: 'center',
-  }
+	AlertFieldContainer: {
+		flexDirection: 'column',
+		width: '90%',
+		margin: '1.5%',
+		paddingHorizontal: 20,
+		paddingVertical: 7,
+		backgroundColor: '#E6F1FF',
+		alignItems: 'center',
+		borderRadius: 15,
+	},
+	fieldTitle: {
+		fontSize: 20,
+		marginBottom: 10,
+		textDecorationLine: 'underline',
+	},
+	descriptionField: {
+		fontSize: 15,
+	},
+	alertDetails: {
+		flexDirection: 'column',
+		fontSize: 10,
+		color: '#8892B0',
+		alignItems: 'center',
+	},
 })
 
 export default function ViewAlert({ title, description }) {
-  return (
-    <View style={styles.AlertFieldContainer}>
-      <Text style={styles.fieldTitle}>{title}</Text>
-      <View style={styles.alertDetails}>
-        <Text style={styles.alertDetails}>{'{time}'}</Text>
-        <Text style={styles.alertDetails}>{'{location}'}</Text>
-      </View>
-      <Text style={styles.descriptionField}>{description}</Text>
-    </View>
-  )
+	return (
+		<View style={styles.AlertFieldContainer}>
+			<Text style={styles.fieldTitle}>{title}</Text>
+			<View style={styles.alertDetails}>
+				<Text style={styles.alertDetails}>{el.time}</Text>
+				<Text style={styles.alertDetails}>{el.location}</Text>
+			</View>
+			<Text style={styles.descriptionField}>{description}</Text>
+		</View>
+	)
 }
